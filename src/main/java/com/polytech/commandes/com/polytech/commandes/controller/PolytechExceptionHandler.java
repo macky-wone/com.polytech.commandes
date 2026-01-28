7a1d11edecfeed7b7e77f6dcff2f3ec128575c9a
Macky Mamadou WONE <wonemacky@gmail.com>
2026-01-28 07:53:09 +0000

    1er commit pour aujourd'hui

--- src/main/java/com/polytech/commandes/com/polytech/commandes/controller/PolytechExceptionHandler.java
+++ src/main/java/com/polytech/commandes/com/polytech/commandes/controller/PolytechExceptionHandler.java
@@ -1,12 +1,13 @@
-package com.polytech.commandes.com.polytech.commandes.controller;
+
 
 import com.polytech.commandes.com.polytech.commandes.service.PolytechApiError;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@ControllerAdvice
+// Intentionally not a @ControllerAdvice to avoid springdoc incompatibility during /v3/api-docs
+// Re-enable global exception handling after updating springdoc to a compatible version
+
 public class PolytechExceptionHandler {
 
     @ExceptionHandler(PolytechApiError.class)
